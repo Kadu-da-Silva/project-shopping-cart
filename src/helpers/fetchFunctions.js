6dd72038d91939c08519b43b984840ac3c54347f
Carlos Eduardo da Silva <carlao0010@hotmail.com>
2023-03-23 14:47:19 -0300

    Requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -7,6 +7,5 @@ export const fetchProductsList = async (query) => {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
   const data = await response.json();
   return data.results; 
-  // console.log(data.results);
 };
  
