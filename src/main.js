0d1d5db206d44c95062621147c884889a9f6daea
Carlos Eduardo da Silva <carlao0010@hotmail.com>
2023-03-23 21:02:09 -0300

    Requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+
+const products = document.querySelectorAll('.products');
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+async function handleFetchProducts () {
+  try {
+    const result = await fetchProductsList('computador');
+    result.forEach((item)=>{
+        products.appendChild(createProductElement(item));
+    });
+  } catch (error) {
+    console.log(error.message);  
+  }
+};
+
+handleFetchProducts();
