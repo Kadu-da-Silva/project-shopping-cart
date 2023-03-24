db1cdb4f1f699f67a3db6a98879686ce9a0a31bd
Carlos Eduardo da Silva <carlao0010@hotmail.com>
2023-03-24 12:50:18 -0300

    Requisito 5

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
-const products = document.querySelector('.products');
+const secProduct = document.querySelector('.products');
 const msgLoading = document.querySelector('.loading');
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
@@ -11,9 +11,13 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 try {
   const result = await fetchProductsList('computador');
   result.forEach((item) => {
-    products.appendChild(createProductElement(item));
+    secProduct.appendChild(createProductElement(item));
   });
 } catch (error) {
+  const msgError = document.createElement('h2');
+  msgError.classList.add('error');
+  msgError.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  secProduct.appendChild(msgError);
   console.log(error.message);
 } finally {
   msgLoading.remove();
