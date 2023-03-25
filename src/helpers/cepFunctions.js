a307839dde9c948b5c835a08a4e242db0497526c
Carlos Eduardo da Silva <carlao0010@hotmail.com>
2023-03-25 19:35:05 -0300

    Requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,18 +1,18 @@
 const inputCep = document.querySelector('.cart__address');
 
 export const getAddress = (query) => {
-  const url_1 = `https://cep.awesomeapi.com.br/json/${query}`;
-  const url_2 = `https://brasilapi.com.br/api/cep/v2/${query}`;
-  
-  const promise_1 = fetch(url_1).then((response) => response.json());
-  const promise_2 = fetch(url_2).then((response) => response.json());
+  const url1 = `https://cep.awesomeapi.com.br/json/${query}`;
+  const url2 = `https://brasilapi.com.br/api/cep/v2/${query}`;
 
-  const promisses = [promise_1, promise_2];
+  const promise1 = fetch(url1).then((response) => response.json());
+  const promise2 = fetch(url2).then((response) => response.json());
+
+  const promisses = [promise1, promise2];
 
   return Promise.any(promisses)
     .then((data) => {
       const { address, distract, city, state } = data;
-      inputCep.textContent = `${address} - ${distract} - ${city} - ${state}`
+      inputCep.textContent = `${address} - ${distract} - ${city} - ${state}`;
     });
 };
 
